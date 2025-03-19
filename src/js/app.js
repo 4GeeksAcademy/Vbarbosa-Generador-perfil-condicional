b253f563c1c18c7dcce85204486bdd1f416edb7c
Vbarbosa <131600563+VitoriaBSouza@users.noreply.github.com>
2025-03-19 20:58:01 +0000

    Edit website body with querySelector variables

--- src/js/app.js
+++ src/js/app.js
@@ -33,15 +33,62 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+
+          <h1>${
+            variables.name && variables.lastName
+              ? variables.name + " " + variables.lastName
+              : variables.name
+              ? variables.name
+              : variables.lastName
+              ? variables.lastName
+              : "No Profile Details"
+          } </h1>
+
+          <h2>${variables.role ? variables.role : "No role assigned"}</h2>
+
+          <h3>${
+            variables.city && variables.country
+              ? variables.city + ", " + variables.country
+              : variables.city
+              ? variables.city
+              : variables.country
+              ? variables.country
+              : "No location details"
+          }
+          </h3>
+
+          <ul class= ${
+            variables.socialMediaPosition == "Right"
+              ? "position-right"
+              : variables.socialMediaPosition
+          }>
+
+            <li><a href="https://twitter.com/${
+              variables.twitter
+                ? "https://twitter.com/" + variables.twitter
+                : "https://twitter.com"
+            }"><i class="fab fa-twitter"></i></a></li>
+            
+            <li><a href="https://github.com/${
+              variables.github
+                ? "https://github.com/" + variables.github
+                : "https://github.com"
+            }"><i class="fab fa-github"></i></a></li>
+
+            <li><a href="https://linkedin.com/school/${
+              variables.linkedin
+                ? "https://linkedin.com/school/" + variables.linkedin
+                : "https://linkedin.com"
+            }"><i class="fab fa-linkedin"></i></a></li>
+
+            <li><a href="${
+              variables.instagram
+                ? "https://instagram.com/" + variables.instagram
+                : "https://instagram.com"
+            }"><i class="fab fa-instagram"></i></a></li>
+
           </ul>
+
         </div>
     `;
 }
